18aba6244baec567133dcd0b09e9a1ebd979be0d
Adam Morris <amorris@mistermorris.com>
2017-04-11 21:09:12 +0800

    tests

---
+++ test/02_tests_functional.js
@@ -0,0 +1,14 @@
+(function () {
+	'use strict';
+
+	describe('User loads main page', function () {
+
+		describe('submits new button', function () {
+
+			it("should save to spreadsheet");
+
+		});
+
+	});
+
+})();
